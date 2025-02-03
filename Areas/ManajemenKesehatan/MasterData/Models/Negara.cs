0aac45c9d17e03d94ee714cc7b7ac92639c5a526
yasmina04 <englishrocker07@gmail.com>
2025-02-03 16:00:40 +0700

    nambah master data negara

---
+++ Areas/ManajemenKesehatan/MasterData/Models/Negara.cs
@@ -0,0 +1,16 @@
+﻿using QuilvianSystemBackendDev.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.Models
+
+{
+    [Table("MstNegara", Schema = "dbo")]
+    public class Negara : UserActivity
+    {
+        [Key]
+        public Guid NegaraId { get; set; }
+        public string KodeNegara { get; set; }
+        public string NamaNegara { get; set; }
+    }
+}
