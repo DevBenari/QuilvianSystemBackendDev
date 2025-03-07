a5eb6caf1891d0d03ba5f7fe385c5c1131ea7749
BenariDev02 <BenariDev02@DESKTOP-BPNS9HK>
2025-03-07 14:46:59 +0700

    Asuransi Pasien

--- Repositories/ApplicationDbContext.cs
+++ Repositories/ApplicationDbContext.cs
@@ -48,6 +48,7 @@ namespace QuilvianSystemBackendDev.Repositories
         public DbSet<DokterPoli> DokterPolis { get; set; }
         public DbSet<CoveranAsuransi> CoveranAsuransis { get; set; }
         public DbSet<Operasi> Operasis { get; set; }
+        public DbSet<AsuransiPasien> AsuransiPasiens { get; set; }
 
         #endregion
 
