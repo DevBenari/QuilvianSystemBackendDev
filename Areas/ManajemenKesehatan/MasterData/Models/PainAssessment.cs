0b550d082111932578bae080c18f0fe9a30021fb
BenariDev02 <BenariDev02@DESKTOP-BPNS9HK>
2025-05-09 14:51:24 +0700

    false default pain asesment

--- Areas/ManajemenKesehatan/MasterData/Models/PainAssessment.cs
+++ Areas/ManajemenKesehatan/MasterData/Models/PainAssessment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using QuilvianSystemBackendDev.Models;
 
@@ -11,21 +12,37 @@ namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.Models
         public Guid PainAssessmentId { get; set; }
         public Guid? KunjunganId { get; set; }
         public string? KeluhanUtama { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsPain { get; set; }
+
         public string? Pemicu { get; set; }
         public string? Kualitas { get; set; }
         public string? Lokasi { get; set; }
         public Guid? SkalaPainId { get; set; }
         public string? Frekuensi { get; set; }
         public string? PainManagement { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsInheritedDisease { get; set; }
+
         public string? InheritedDisease { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsAlergic { get; set; }
+
         public string? Alergic { get; set; }
         public string? NafsuMakan { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsMual { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsMuntah { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsFallRisk { get; set; }
+
         public string? FallRisk { get; set; }
     }
 }
--- Areas/ManajemenKesehatan/MasterData/ViewModels/PainAssessmentViewModel.cs
+++ Areas/ManajemenKesehatan/MasterData/ViewModels/PainAssessmentViewModel.cs
@@ -1,24 +1,41 @@
-﻿namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.ViewModels
+﻿using System.ComponentModel;
+
+namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.ViewModels
 {
     public class PainAssessmentViewModel
     {
         public Guid? KunjunganId { get; set; }
         public string? KeluhanUtama { get; set; }
+        [DefaultValue(false)]
         public bool? IsPain { get; set; }
+
         public string? Pemicu { get; set; }
         public string? Kualitas { get; set; }
         public string? Lokasi { get; set; }
         public Guid? SkalaPainId { get; set; }
         public string? Frekuensi { get; set; }
         public string? PainManagement { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsInheritedDisease { get; set; }
+
         public string? InheritedDisease { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsAlergic { get; set; }
+
         public string? Alergic { get; set; }
         public string? NafsuMakan { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsMual { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsMuntah { get; set; }
+
+        [DefaultValue(false)]
         public bool? IsFallRisk { get; set; }
+
         public string? FallRisk { get; set; }
     }
 }
