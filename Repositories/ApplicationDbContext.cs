3ec74c35b83450635f8787c0e36b65d784d3dd1b
yasmina04 <englishrocker07@gmail.com>
2025-05-20 14:51:20 +0700

    fix

--- Repositories/ApplicationDbContext.cs
+++ Repositories/ApplicationDbContext.cs
@@ -48,7 +48,6 @@ namespace QuilvianSystemBackendDev.Repositories
         public DbSet<DokterPoli> DokterPolis { get; set; }
         public DbSet<CoveranAsuransi> CoveranAsuransis { get; set; }
         public DbSet<Operasi> Operasis { get; set; }
-        public DbSet<DokterSubPoli> DokterSubPolis { get; set; }
         public DbSet<AsuransiPasien> AsuransiPasiens { get; set; }
         public DbSet<KodePos> KodePoss { get; set; }
         public DbSet<Suku> Sukus { get; set; }
