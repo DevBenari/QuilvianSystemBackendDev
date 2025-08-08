45246de19237265a9a79923d250bf057b38bf56c
yasmina04 <englishrocker07@gmail.com>
2025-08-08 13:50:34 +0700

    fix

--- Areas/ManajemenKesehatan/MasterData/Enum/EnumJenisKunjungan.cs
+++ Areas/ManajemenKesehatan/MasterData/Enum/EnumJenisKunjungan.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.Models;
 
 namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.Enum
 {
-
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum EnumJenisKunjungan
     {
         [Display(Name = "IP")]
