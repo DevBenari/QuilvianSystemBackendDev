4dc8c9c7c5252f8f2a3012b8251bd208832cec2b
yasmina04 <englishrocker07@gmail.com>
2025-08-26 13:23:38 +0700

    Add table SDKI Observasi

---
+++ Areas/ManajemenKesehatan/RawatInap/Models/SDKIObservasi.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QuilvianSystemBackendDev.Models;
+
+namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.RawatInap.Models
+{
+    public class SDKIObservasi : UserActivity
+    {
+        [Key]
+        public Guid SDKIObservasiId { get; set; }
+        public Guid? SDKIDiagnosaId { get; set; }
+        public string? NamaObservasi { get; set; }
+        public string? Keterangan { get; set; }
+    }
+}
