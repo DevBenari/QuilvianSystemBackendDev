3043ed23ffbe43632bb6dce421da6f3c35107925
yasmina04 <englishrocker07@gmail.com>
2025-09-23 11:51:02 +0700

    fix

--- Areas/ManajemenKesehatan/Farmasi/Controllers/PermintaanUnitController.cs
+++ Areas/ManajemenKesehatan/Farmasi/Controllers/PermintaanUnitController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Globalization;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Farmasi.HubSignalR;
 using QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Farmasi.Models;
 using QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Farmasi.ViewModels;
 using QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Kasir.Controllers;
@@ -31,19 +34,23 @@ namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Farmasi.Controllers
 
         private readonly ILogger<PermintaanUnitController> _logger;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHubContext<PermintaanUnitHub> _hubContext;
 
         public PermintaanUnitController(
             ApplicationDbContext applicationDbContext,
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             ILogger<PermintaanUnitController> logger,
-            IWebHostEnvironment webHostEnvironment)
+            IWebHostEnvironment webHostEnvironment,
+            IHubContext<PermintaanUnitHub> hubContext
+            )
         {
             _applicationDbContext = applicationDbContext;
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _webHostEnvironment = webHostEnvironment;
+            _hubContext = hubContext;
         }
 
         private DateTime? TryParseTanggalToUtc(string tanggal)
@@ -268,6 +275,11 @@ namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Farmasi.Controllers
                 await _applicationDbContext.DetailPermintaanUnits.AddRangeAsync(detailItems);
 
                 int result = await _applicationDbContext.SaveChangesAsync();
+                await _hubContext.Clients.All.SendAsync("PermintaanUnitCreated", new
+                {
+                    Action = "create",
+                    PermintaanUnitId = data.PermintaanUnitId
+                });
 
                 if (result > 0)
                 {
@@ -440,7 +452,11 @@ namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Farmasi.Controllers
 
                 // Simpan semua perubahan dalam satu transaksi
                 int result = await _applicationDbContext.SaveChangesAsync();
-
+                await _hubContext.Clients.All.SendAsync("PermintaanUnitUpdate", new
+                {
+                    Action = "update",
+                    PermintaanUnitId = data.PermintaanUnitId
+                });
                 if (result > 0)
                 {
                     return Ok(new { message = "Update Data Berhasil || 200 OK" });
