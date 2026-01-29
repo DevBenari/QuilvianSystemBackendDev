c71e71034c0ebb59ed64180c7e118ebdaf3d88fe
BenariDev02 <BenariDev02@DESKTOP-BPNS9HK>
2026-01-29 16:17:39 +0700

    update

---
+++ Migrations/20260129091500_poupaddnew.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuilvianSystemBackendDev.Migrations
+{
+    public partial class poupaddnew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TanggalTerimaInvoice",
+                schema: "public",
+                table: "PurchaseOrder");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAccess",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TermOfPayment",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusPO",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestType",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RemainingDay",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "QtyTotal",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PurchaseRequestNumber",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PurchaseOrderNumber",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Keterangan",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GrandTotal",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiredDate",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InvoiceDate",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupplierCode",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceDate",
+                schema: "public",
+                table: "PurchaseOrder");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierCode",
+                schema: "public",
+                table: "PurchaseOrder");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAccess",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TermOfPayment",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusPO",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestType",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RemainingDay",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "QtyTotal",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PurchaseRequestNumber",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PurchaseOrderNumber",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Keterangan",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GrandTotal",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiredDate",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TanggalTerimaInvoice",
+                schema: "public",
+                table: "PurchaseOrder",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
