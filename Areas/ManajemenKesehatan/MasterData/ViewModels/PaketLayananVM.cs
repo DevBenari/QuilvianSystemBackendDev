4684f1996075e5e15cb73b8159fced1ba4dd915a
yasmina04 <englishrocker07@gmail.com>
2026-03-14 13:59:06 +0700

    add controller paket layanan

---
+++ Areas/ManajemenKesehatan/MasterData/ViewModels/PaketLayananVM.cs
@@ -0,0 +1,14 @@
+﻿namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.ViewModels
+{
+    public class PaketLayananVM
+    {
+        public string? KodePaketLayanan { get; set; }
+        public string? NamaPaketLayanan { get; set; }
+        public DateTime? TglPembuatan { get; set; }
+        public Guid? LayananId { get; set; }
+        public string? Keterangan { get; set; }
+
+        public List<PaketLayananDetailVM>? Details { get; set; }
+
+    }
+}
