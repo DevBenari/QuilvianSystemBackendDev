f6f8357d936e345badead2f8720b72c8fb58e1a0
yasmina04 <englishrocker07@gmail.com>
2026-03-26 16:17:17 +0700

    fix

--- Areas/ManajemenKesehatan/Laboratorium/Controllers/LabBookingController.cs
+++ Areas/ManajemenKesehatan/Laboratorium/Controllers/LabBookingController.cs
@@ -84,6 +84,7 @@ namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Laboratorium.Control
             return null;
         }
 
+
         [HttpGet]
         public async Task<IActionResult> GetAll(int page = 1, int perPage = 10)
         {
