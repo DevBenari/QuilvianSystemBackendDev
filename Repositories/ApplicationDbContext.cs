605444d3aa610daf67f1c6e0040cbac6408024ad
yasmina04 <englishrocker07@gmail.com>
2026-04-08 15:05:01 +0700

    add table diskon approved

--- Repositories/ApplicationDbContext.cs
+++ Repositories/ApplicationDbContext.cs
@@ -250,6 +250,7 @@ namespace QuilvianSystemBackendDev.Repositories
         public DbSet<PaketLayananAsuransi> PaketLayananAsuransis { get; set; }
         public DbSet<PaketLayananDiskon> PaketLayananDiskons { get; set; }
         public DbSet<LoketKasir> LoketKasirs { get; set; }
+        public DbSet<DiskonApproved> DiskonApproveds { get; set; }
         #endregion
 
         #region Areas Keuangan
