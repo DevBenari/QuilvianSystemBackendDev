674a6c86fb5416ed792a8eb7db975738e5fa9f58
BenariDev02 <BenariDev02@DESKTOP-BPNS9HK>
2026-04-16 12:19:35 +0700

    end karya

--- Areas/Administrator/MasterData/Models/UserActive.cs
+++ Areas/Administrator/MasterData/Models/UserActive.cs
@@ -28,5 +28,26 @@ namespace QuilvianSystemBackendDev.Areas.Administrator.MasterData.Models
         public Guid? PositionId { get; set; }
         public Guid? TipeUserId { get; set; }
         public Guid? InstalasiUnitId { get; set; }
+
+
+        //karyawan
+        //public Guid KaryawanId { get; set; }
+        public Guid? DepartementId { get; set; }
+        public Guid? JabatanId { get; set; }
+
+        public string? NoIdentitas { get; set; }
+        public string? KodeKaryawan { get; set; }
+        public string? NoRekening { get; set; }
+        public string? NoKaryawan { get; set; }
+        public string? BankId { get; set; }
+
+        public DateTime? TanggalKontrak { get; set; }
+        public DateTime? TanggalAwalKerja { get; set; }
+        public DateTime? TanggalAkhirKerja { get; set; }
+
+        public string? NoHandphone { get; set; }
+        public string? Alamat { get; set; }
+        public string? FotoPath { get; set; }
+        public string? FotoName { get; set; }
     }
 }
