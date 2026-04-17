14a7f5e3362ccc51169a87680da9dd294842d50c
yasmina04 <englishrocker07@gmail.com>
2026-04-17 11:06:37 +0700

    add table ttg tarif

---
+++ Areas/ManajemenKesehatan/MasterData/Models/TarifHemodialisa.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QuilvianSystemBackendDev.Models;
+
+namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.Models
+{
+    public class TarifHemodialisa : UserActivity
+    {
+        [Key]
+        public Guid TarifHemodialisaId { get; set; }
+        public Guid? LabPemeriksaanId { get; set; }
+        public Guid? KelasId { get; set; }
+        public decimal? TarifDokter { get; set; }
+        public decimal? TarifRs { get; set; }
+        public decimal? TarifJp { get; set; }
+        public decimal? TarifBahp { get; set; }
+        public decimal? TarifLain { get; set; }
+        public decimal? TarifTotal { get; set; }
+        public decimal? KSO { get; set; }
+        public string? Keterangan { get; set; }
+    }
+}
---
+++ Areas/ManajemenKesehatan/MasterData/Models/TarifKamar.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using QuilvianSystemBackendDev.Models;
+
+namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.Models
+{
+    public class TarifKamar : UserActivity
+    {
+        [Key]
+        public Guid TarifKamarId { get; set; }
+        public Guid? KamarId { get; set; }
+        public Guid? KelasId { get; set; }
+        public decimal? TarifDokter { get; set; }
+        public decimal? TarifRs { get; set; }
+        public decimal? TarifJp { get; set; }
+        public decimal? TarifBahp { get; set; }
+        public decimal? TarifLain { get; set; }
+        public decimal? TarifTotal { get; set; }
+        public decimal? KSO { get; set; }
+        public string? Keterangan { get; set; }
+    }
+}
