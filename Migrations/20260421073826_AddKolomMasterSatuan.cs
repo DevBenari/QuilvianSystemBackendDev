030808659a6b079facf6ac99576ddc08cd3bf50e
yasmina04 <englishrocker07@gmail.com>
2026-04-21 15:27:12 +0700

    add kolom di naster satuan

---
+++ Migrations/20260421073826_AddKolomMasterSatuan.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuilvianSystemBackendDev.Migrations
+{
+    public partial class AddKolomMasterSatuan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NamaSatuan",
+                schema: "public",
+                table: "MstSatuan",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KodeSatuan",
+                schema: "public",
+                table: "MstSatuan",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SingkatanSatuan",
+                schema: "public",
+                table: "MstSatuan",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SingkatanSatuan",
+                schema: "public",
+                table: "MstSatuan");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NamaSatuan",
+                schema: "public",
+                table: "MstSatuan",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KodeSatuan",
+                schema: "public",
+                table: "MstSatuan",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
