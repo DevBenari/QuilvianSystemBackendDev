6d5a9a08e22508f94698c2d54407c53ca259d332
yasmina04 <englishrocker07@gmail.com>
2026-06-25 16:24:40 +0700

    add table faskes dan LabRujukan

---
+++ Areas/ManajemenKesehatan/Laboratorium/Models/LabRujukan.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using QuilvianSystemBackendDev.Areas.ManajemenKesehatan.MasterData.Models;
+using QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Pendaftaran.Models;
+using QuilvianSystemBackendDev.Models;
+
+namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Laboratorium.Models
+{
+    public class LabRujukan : UserActivity
+    {
+        [Key]
+        public Guid LabRujukanId { get; set; }
+        public Guid? KunjunganId { get; set; }
+        public Guid? PasienId { get; set; }
+        public Guid? LabId { get; set; }
+        public string? ArahRujukan { get; set; }
+        public Guid? FaskesRujukanId { get; set; }
+        public string? DokterPerujuk { get; set; }
+        public string? TglRujukan { get; set; }
+        public string? Keterangan { get; set; }
+
+        [ForeignKey(nameof(KunjunganId))]
+        public virtual Kunjungan? Kunjungan { get; set; }
+
+        [ForeignKey(nameof(PasienId))]
+        public virtual PendaftaranPasienBaru? Pasien { get; set; }
+
+        [ForeignKey(nameof(LabId))]
+        public virtual Lab? Lab { get; set; }
+
+        [ForeignKey(nameof(FaskesRujukanId))]
+        public virtual FaskesRujukan? FaskesRujukan { get; set; }
+    }
+}
---
+++ Migrations/20260625084802_AddTableLabRujukanDanFaskes.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuilvianSystemBackendDev.Migrations
+{
+    public partial class AddTableLabRujukanDanFaskes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MstFaskesRujukan",
+                columns: table => new
+                {
+                    FaskesRujukanId = table.Column<Guid>(type: "uuid", nullable: false),
+                    NamaFaskesRujukan = table.Column<string>(type: "text", nullable: true),
+                    AlamatFaskesRujukan = table.Column<string>(type: "text", nullable: true),
+                    NoTelpFaskesRujukan = table.Column<string>(type: "text", nullable: true),
+                    Keterangan = table.Column<string>(type: "text", nullable: true),
+                    CreateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreateBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    UpdateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdateBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    DeleteDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DeleteBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsDelete = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MstFaskesRujukan", x => x.FaskesRujukanId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LabRujukans",
+                columns: table => new
+                {
+                    LabRujukanId = table.Column<Guid>(type: "uuid", nullable: false),
+                    KunjunganId = table.Column<Guid>(type: "uuid", nullable: true),
+                    PasienId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LabId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ArahRujukan = table.Column<string>(type: "text", nullable: true),
+                    FaskesRujukanId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DokterPerujuk = table.Column<string>(type: "text", nullable: true),
+                    TglRujukan = table.Column<string>(type: "text", nullable: true),
+                    Keterangan = table.Column<string>(type: "text", nullable: true),
+                    CreateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreateBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    UpdateDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdateBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    DeleteDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DeleteBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsDelete = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LabRujukans", x => x.LabRujukanId);
+                    table.ForeignKey(
+                        name: "FK_LabRujukans_MstFaskesRujukan_FaskesRujukanId",
+                        column: x => x.FaskesRujukanId,
+                        principalTable: "MstFaskesRujukan",
+                        principalColumn: "FaskesRujukanId");
+                    table.ForeignKey(
+                        name: "FK_LabRujukans_MstKunjungan_KunjunganId",
+                        column: x => x.KunjunganId,
+                        principalSchema: "public",
+                        principalTable: "MstKunjungan",
+                        principalColumn: "KunjunganID");
+                    table.ForeignKey(
+                        name: "FK_LabRujukans_MstLab_LabId",
+                        column: x => x.LabId,
+                        principalSchema: "public",
+                        principalTable: "MstLab",
+                        principalColumn: "LabId");
+                    table.ForeignKey(
+                        name: "FK_LabRujukans_PdfPasienBaru_PasienId",
+                        column: x => x.PasienId,
+                        principalSchema: "public",
+                        principalTable: "PdfPasienBaru",
+                        principalColumn: "PendaftaranPasienBaruId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabRujukans_FaskesRujukanId",
+                table: "LabRujukans",
+                column: "FaskesRujukanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabRujukans_KunjunganId",
+                table: "LabRujukans",
+                column: "KunjunganId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabRujukans_LabId",
+                table: "LabRujukans",
+                column: "LabId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabRujukans_PasienId",
+                table: "LabRujukans",
+                column: "PasienId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LabRujukans");
+
+            migrationBuilder.DropTable(
+                name: "MstFaskesRujukan");
+        }
+    }
+}
