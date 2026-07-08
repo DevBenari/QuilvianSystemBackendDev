8adda8a19dc1dcab2de6eb18b624e636e8c006bd
yasmina04 <englishrocker07@gmail.com>
2026-07-08 16:11:16 +0700

    Tambah kolom karyawan id

---
+++ Migrations/20260708080155_AddKolomKaryawanIdDiPdfpasienBaru.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuilvianSystemBackendDev.Migrations
+{
+    public partial class AddKolomKaryawanIdDiPdfpasienBaru : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "KaryawanId",
+                schema: "public",
+                table: "PdfPasienBaru",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KaryawanId",
+                schema: "public",
+                table: "PdfPasienBaru");
+
+        }
+    }
+}
