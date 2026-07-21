d95af81bae920c026502c22ebddd6b59a9efbc22
BenariDev02 <BenariDev02@DESKTOP-BPNS9HK>
2026-07-21 12:49:48 +0700

    Perbiakna

---
+++ Areas/Finance/COA/ViewModels/COAMappingViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuilvianSystemBackendDev.Areas.Finance.COA.ViewModels
+{
+    public class COAMappingViewModel
+    {
+        public Guid TransaksiId { get; set; }
+
+        /// <summary>
+        /// Nama transaksi, diambil berdasarkan TransaksiId
+        /// </summary>
+        [MaxLength(200)]
+        public string? NamaTransaksi { get; set; }
+
+        /// <summary>
+        /// Referensi ke Master COA
+        /// </summary>
+        public Guid COAId { get; set; }
+
+        /// <summary>
+        /// Nama COA, diambil berdasarkan COAId
+        /// </summary>
+        [MaxLength(200)]
+        public string? NamaCOA { get; set; }
+
+        /// <summary>
+        /// Keterangan
+        /// </summary>
+        [MaxLength(500)]
+        public string? Keterangan { get; set; }
+    }
+}
