9f6bbd6822ac1cf08762947fa6efea3fd71c7022
yasmina04 <englishrocker07@gmail.com>
2026-07-31 14:33:20 +0700

    ubah jenis variabel satuan pemeriksaan

---
+++ Migrations/20260731071931_EditJenisTypeSatuanPemeriksaan.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuilvianSystemBackendDev.Migrations
+{
+    public partial class EditJenisTypeSatuanPemeriksaan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SatuanPemeriksaan",
+                table: "LabHasilDetails",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SatuanPemeriksaan",
+                table: "LabHasilDetails",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
