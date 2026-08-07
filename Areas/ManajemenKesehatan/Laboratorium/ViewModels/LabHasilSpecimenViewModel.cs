1ee0182524255fc4d60c766823fa30cedea7ee26
yasmina04 <englishrocker07@gmail.com>
2026-08-07 14:43:08 +0700

    add controller lab hasil specimen

---
+++ Areas/ManajemenKesehatan/Laboratorium/ViewModels/LabHasilSpecimenViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace QuilvianSystemBackendDev.Areas.ManajemenKesehatan.Laboratorium.ViewModels
+{
+    public class LabHasilSpecimenViewModel
+    {
+        public Guid? LabHasilId { get; set; }
+        public Guid? KunjunganId { get; set; }
+        public Guid? PasienId { get; set; }
+        public Guid? AsalSpecimenId { get; set; }
+        public Guid? JenisSpecimenId { get; set; }
+    }
+}
